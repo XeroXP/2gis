8b80de28975a8432ecd796252eefc6e5ae41ed6c
Maksim <vl-mail25@yandex.ru>
2017-10-04 05:17:21 +0700

    Add REST controllers

---
+++ modules/v1/controllers/BuildingController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+
+/**
+ * Building controller for the `v1` module
+ */
+class BuildingController extends ActiveController
+{
+
+    public $modelClass = 'app\modules\api\models\Building';
+
+}
---
+++ modules/v1/controllers/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+
+/**
+ * Category controller for the `v1` module
+ */
+class CategoryController extends ActiveController
+{
+
+    public $modelClass = 'app\modules\api\models\Category';
+
+}
