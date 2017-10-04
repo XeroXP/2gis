13b04e0982003766cacd3193c47ef5f0ec03bbf7
Maksim <vl-mail25@yandex.ru>
2017-10-04 19:46:43 +0700

    add USER module

---
+++ modules/user/controllers/DefaultController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace app\modules\user\controllers;
+
+use yii\web\Controller;
+use app\modules\user\models\LoginForm;
+use yii\web\Response;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use Yii;
+
+/**
+ * Default controller for the `user` module
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+
+
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+}
